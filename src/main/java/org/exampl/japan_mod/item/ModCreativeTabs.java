8cc8cec83d0799020815e5845921f093b3f8a4af
SimobiSirOP <130081705+SimobiSirOP@users.noreply.github.com>
2024-04-25 21:34:47 +0300

    Update ModCreativeTabs.java

--- src/main/java/org/exampl/japan_mod/item/ModCreativeTabs.java
+++ src/main/java/org/exampl/japan_mod/item/ModCreativeTabs.java
@@ -11,9 +11,13 @@ import org.exampl.japan_mod.Japan_mod;
 import org.exampl.japan_mod.block.ModBlocks;
 
 public class ModCreativeTabs {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Japan_mod.MODID);
-    public static final RegistryObject<CreativeModeTab> JAPAN_MOD_TAB = CREATIVE_TAB.register("japan_mod_tab", () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.RICE_BOWL.get())).title(Component.translatable("creative_tab_japan_mod")).displayItems((pDisplayParameters, pItemOutput) -> {
-        pItemOutput.accept(ModItems.LOTOS.get());
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TAB = 
+    DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Japan_mod.MODID);
+    public static final RegistryObject<CreativeModeTab> JAPAN_MOD_TAB = CREATIVE_TAB.register("japan_mod_tab", 
+        () -> CreativeModeTab.builder().icon(
+        () -> new ItemStack(ModItems.RICE_BOWL.get()))
+        .title(Component.translatable("creative_tab_japan_mod"))
+        .displayItems((pDisplayParameters, pItemOutput) -> {
         pItemOutput.accept(ModBlocks.PIJERYS.get());
         pItemOutput.accept(ModBlocks.GIBISKUS.get());
         pItemOutput.accept(ModBlocks.LAVENDER.get());
